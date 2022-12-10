2b25f806913817314a253df842fc86648f5f3d35
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2022-12-10 12:42:44 +0500

    v1.0.1

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g5#(^joc1zv8!4o!a-c*p(7(9gsh^r-tc=s#snsi+gh7p)8&*t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
