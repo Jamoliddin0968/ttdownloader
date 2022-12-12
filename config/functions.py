1cbab7c6395be48682a036c7dbca0fa49aaf6955
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2022-12-12 12:42:31 +0500

    bug fixed

--- config/functions.py
+++ config/functions.py
@@ -54,7 +54,8 @@ class TTDonwloadView(APIView):
                 raise Http404
             downloadsoup = BeautifulSoup(response.text,"lxml")
             downlink = downloadsoup.a["href"]
-            if 'ssstik.io' not in downlink:
+            print(downlink)
+            if 'ssstik' not in downlink:
                 raise Http404
             return Response({"link":downlink})
         except :
